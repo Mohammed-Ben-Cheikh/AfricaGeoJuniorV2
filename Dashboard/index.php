4e44289031f45bda11abe25dc56143b713f1304b
Mohammed Ben Cheikh <bencheikh.official@gmail.com>
2024-12-26 16:26:21 +0100

    Add initial project files including HTML templates and configuration

---
+++ Dashboard/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css" rel="stylesheet" />
+    <link rel="stylesheet" href="../public/css/style.css">
+    <title></title>
+</head>
+
+<body>
+    
+</body>
+
+</html>
