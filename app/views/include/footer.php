70ae27dda9d5175f20b4371a0e99813a873fdb15
BensaltanaHoussam <bensaltna07@gmail.com>
2024-12-27 09:51:58 +0100

    inisial commit

---
+++ app/views/include/footer.php
@@ -0,0 +1,44 @@
+
+    <!-- FOOTER -->
+    <footer class="bg-slate-100">
+      <div
+        class="container flex justify-around items-center p-8 flex-col md:flex-row"
+      >
+    
+      <div class="bg-white p-4 flex flex-col items-center">
+        <img
+        class="w-40 pb-2 "
+        src="./img/Black White Stylish Minimalist Small World Logo.png"
+        alt="logo"
+      />
+      <p class="text-xs w-32 text-center text-black">Thank you for visiting our website! We appreciate your time and support. If you have any questions or feedback, feel free to reach out. We look forward to having you back soon!</p>
+      
+      </div>
+       
+      
+        <div class="flex gap-28 text-black md:flex-row">
+          <div class="mb-4">
+            <h1 class="text-xl font-medium pb-2">Quick Links</h1>
+            <ul>
+              <li class="pb-1 hover:underline transition-all duration-400 hover:cursor-pointer"> <a href="#">Home</a></li>
+              <li class="pb-1 hover:underline transition-all duration-400 hover:cursor-pointer"><a href="#">About Us</a></li>
+              <li class="pb-1 hover:underline transition-all duration-400 hover:cursor-pointer"><a href="#">Services</a></li>
+              <li class="pb-1 hover:underline transition-all duration-400 hover:cursor-pointer"><a href="#">Account</a></li>
+              <li class="pb-1 hover:underline transition-all duration-400 hover:cursor-pointer"><a href="#">Team</a></li>
+            </ul>
+          </div>
+      
+          <div class="mb-4">
+            <h1 class="text-xl font-medium pb-2">Social Media</h1>
+            <ul>
+              <li class="pb-1 hover:underline transition-all duration-400 hover:cursor-pointer"><i class="fa-brands fa-facebook pr-2"></i><a href="#">Facebook</a></li>
+              <a href="#"><li class="pb-1 hover:underline transition-all duration-400 hover:cursor-pointer"><i class="fa-brands fa-instagram pr-2"></i><a href="#">Instagram</a></li>
+              <a href="#"><li class="pb-1 hover:underline transition-all duration-400 hover:cursor-pointer"><i class="fa-brands fa-twitter pr-2"></i><a href="#">Twitter</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <p class="text-green-950 flex justify-end text-xs p-4">
+        2024 Codeshogun. All rights reserved
+      </p>
+    </footer>
